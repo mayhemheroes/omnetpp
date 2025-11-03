mayhemheroes/omnetpp

commit 494c991aab72b79d364df6bc67edac3d6a753e56

scave/python: Drop the now-removed second argument to random.shuffle
This was deprecated in Python 3.9 and removed in Python 3.11.

python/omnetpp/scave/utils.py
@@ -1220,7 +1220,7 @@ def _initialize_cycles(props):
 
     ml = list("osv^<>pDd")
     if seed != 0:
-        random.shuffle(ml, r.random)
+        random.shuffle(ml)
     _marker_cycle = it.cycle(ml)
 
     prop_cycler = plt.rcParams['axes.prop_cycle']
@@ -1232,7 +1232,7 @@ def _initialize_cycles(props):
 
     cl = ["C" + str(i) for i in range(num_colors)]
     if seed != 0:
-        random.shuffle(cl, r.random)
+        random.shuffle(cl)
     _color_cycle = it.cycle(cl)
 
 

commit 420576ec1a5666957e3722d2fa3ab36340a55321

ide/scave: Fix color/marker cycles in charts not being deterministic with seed != 0
This regressed with 494c991.

python/omnetpp/scave/utils.py
@@ -1871,7 +1871,7 @@ def _initialize_cycles(props):
 
     ml = list("osv^<>pDd")
     if seed != 0:
-        random.shuffle(ml)
+        r.shuffle(ml)
     _marker_cycle = it.cycle(ml)
 
     prop_cycler = plt.rcParams['axes.prop_cycle']
@@ -1883,7 +1883,7 @@ def _initialize_cycles(props):
 
     cl = ["C" + str(i) for i in range(num_colors)]
     if seed != 0:
-        random.shuffle(cl)
+        r.shuffle(cl)
     _color_cycle = it.cycle(cl)
 
 