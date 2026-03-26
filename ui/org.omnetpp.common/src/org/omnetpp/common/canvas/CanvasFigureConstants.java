026913875150cce73f50ddbcb7dad3269e2c221c
TÖRÖK Attila <torokati44@gmail.com>
2026-03-26 11:29:05 +0100

    ide/common: Add CanvasFigureConstants class, to be used later
    ... in canvas figure validation, property sources, and rendering/editing.

---
+++ ui/org.omnetpp.common/src/org/omnetpp/common/canvas/CanvasFigureConstants.java
@@ -0,0 +1,123 @@
+package org.omnetpp.common.canvas;
+
+public final class CanvasFigureConstants {
+
+    // figure types
+    public static final String FTYPE_LINE = "line";
+    public static final String FTYPE_POLYLINE = "polyline";
+    public static final String FTYPE_POLYGON = "polygon";
+    public static final String FTYPE_RECTANGLE = "rectangle";
+    public static final String FTYPE_OVAL = "oval";
+    public static final String FTYPE_RING = "ring";
+    public static final String FTYPE_PIESLICE = "pieslice";
+    public static final String FTYPE_ARC = "arc";
+    public static final String FTYPE_GROUP = "group";
+    public static final String FTYPE_PANEL = "panel";
+    public static final String FTYPE_TEXT = "text";
+    public static final String FTYPE_LABEL = "label";
+    public static final String FTYPE_IMAGE = "image";
+    public static final String FTYPE_PIXMAP = "pixmap";
+    public static final String FTYPE_ICON = "icon";
+    public static final String FTYPE_PATH = "path";
+
+    // property keys
+    public static final String PKEY_TYPE = "type";
+    public static final String PKEY_VISIBLE = "visible";
+    public static final String PKEY_TAGS = "tags";
+    public static final String PKEY_TRANSFORM = "transform";
+    public static final String PKEY_TOOLTIP = "tooltip";
+    public static final String PKEY_ZINDEX = "zIndex";
+    public static final String PKEY_BOUNDS = "bounds";
+    public static final String PKEY_POS = "pos";
+    public static final String PKEY_POINTS = "points";
+    public static final String PKEY_ANCHOR = "anchor";
+    public static final String PKEY_ANCHORPOINT = "anchorPoint"; // cPanelFigure
+    public static final String PKEY_SIZE = "size";
+    public static final String PKEY_INNERSIZE = "innerSize";
+    public static final String PKEY_CORNERRADIUS = "cornerRadius";
+    public static final String PKEY_STARTANGLE = "startAngle";
+    public static final String PKEY_ENDANGLE = "endAngle";
+    public static final String PKEY_PATH = "path";
+    public static final String PKEY_SMOOTH = "smooth";
+    public static final String PKEY_OFFSET = "offset";
+    public static final String PKEY_LINECOLOR = "lineColor";
+    public static final String PKEY_LINESTYLE = "lineStyle";
+    public static final String PKEY_LINEWIDTH = "lineWidth";
+    public static final String PKEY_LINEOPACITY = "lineOpacity";
+    public static final String PKEY_CAPSTYLE = "capStyle";
+    public static final String PKEY_JOINSTYLE = "joinStyle";
+    public static final String PKEY_ZOOMLINEWIDTH = "zoomLineWidth";
+    public static final String PKEY_FILLCOLOR = "fillColor";
+    public static final String PKEY_FILLOPACITY = "fillOpacity";
+    public static final String PKEY_FILLRULE = "fillRule";
+    public static final String PKEY_STARTARROWHEAD = "startArrowhead";
+    public static final String PKEY_ENDARROWHEAD = "endArrowhead";
+    public static final String PKEY_TEXT = "text";
+    public static final String PKEY_ALIGN = "align";
+    public static final String PKEY_FONT = "font";
+    public static final String PKEY_COLOR = "color";
+    public static final String PKEY_OPACITY = "opacity";
+    public static final String PKEY_IMAGE = "image";
+    public static final String PKEY_TINT = "tint";
+    public static final String PKEY_INTERPOLATION = "interpolation";
+    public static final String PKEY_RESOLUTION = "resolution"; // cPixmapFigure
+    public static final String PKEY_ANGLE = "angle"; // cLabelFigure
+    public static final String PKEY_HALO = "halo"; // cAbstractTextFigure
+
+    // property values
+    public static final String PVAL_ANCHOR_C = "c";
+    public static final String PVAL_ANCHOR_CENTER = "center";
+    public static final String PVAL_ANCHOR_N = "n";
+    public static final String PVAL_ANCHOR_E = "e";
+    public static final String PVAL_ANCHOR_S = "s";
+    public static final String PVAL_ANCHOR_W = "w";
+    public static final String PVAL_ANCHOR_NW = "nw";
+    public static final String PVAL_ANCHOR_NE = "ne";
+    public static final String PVAL_ANCHOR_SE = "se";
+    public static final String PVAL_ANCHOR_SW = "sw";
+    public static final String PVAL_ANCHOR_START = "start"; // text baseline
+    public static final String PVAL_ANCHOR_MIDDLE = "middle"; // text baseline
+    public static final String PVAL_ANCHOR_END = "end"; // text baseline
+
+    /* // property values - are these necessary?
+    public static final String PVAL_SIMPLE = "simple";
+    public static final String PVAL_TRIANGLE = "triangle";
+    public static final String PVAL_BARBED = "barbed";
+    public static final String PVAL_BEVEL = "bevel";
+    public static final String PVAL_MITER = "miter";
+    public static final String PVAL_FLAT = "flat";
+    public static final String PVAL_ROUND = "round";
+    public static final String PVAL_SQUARE = "square";
+    public static final String PVAL_SOLID = "solid";
+    public static final String PVAL_DASHED = "dashed";
+    public static final String PVAL_DOTTED = "dotted";
+    public static final String PVAL_TRANSLATE = "translate";
+    public static final String PVAL_ROTATE = "rotate";
+    public static final String PVAL_SCALE = "scale";
+    public static final String PVAL_SKEWX = "skewx";
+    public static final String PVAL_SKEWY = "skewy";
+    */
+
+
+    /*
+    some more potential literals to be replaced:
+
+    "false"
+    "true"
+
+    "figure"
+    "submodules"
+
+    "normal"
+    "bold"
+    "underline"
+    "italic"
+
+    "height"
+    "width"
+
+    "left"
+    "center"
+    "right"
+    */
+}
