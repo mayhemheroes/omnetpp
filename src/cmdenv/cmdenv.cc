738a6fcae07c25a55aa18fca6c85f7498f316916
Rudolf Hornig <rudi@omnetpp.org>
2026-05-05 13:28:31 +0200

    common: changed the reported amount of used memory to RSS (resident size)
    - Memory swapped out is no longer included in this memory metric. Only counting the
    privately held data currently in the physical memory.
    - allocated memory is shown only in status updates, but not in every event banner.

--- src/cmdenv/cmdenv.cc
+++ src/cmdenv/cmdenv.cc
@@ -494,8 +494,7 @@ void Cmdenv::printEventBanner(cEvent *event)
         out << "   Elapsed: " << timeToStr(getElapsedSecs())
             << "   Messages: created: " << cMessage::getTotalMessageCount()
             << "  present: " << cMessage::getLiveMessageCount()
-            << "  in FES: " << getSimulation()->getFES()->getLength()
-            << "  Memory: " << UnitConversion::formatInBestUnit(opp_memory_usage(), "B") << "\n"; // note: "\n" not endl, because we don't want auto-flush on each event
+            << "  in FES: " << getSimulation()->getFES()->getLength() << "\n"; // note: "\n" not endl, because we don't want auto-flush on each event
     }
 }
 
@@ -517,14 +516,14 @@ void Cmdenv::doStatusUpdate(Speedometer& speedometer)
         out << "     Messages:  created: " << cMessage::getTotalMessageCount()
             << "   present: " << cMessage::getLiveMessageCount()
             << "   in FES: " << getSimulation()->getFES()->getLength()
-            << "   Memory: " << UnitConversion::formatInBestUnit(opp_memory_usage(), "B") << endl;
+            << "   Memory (rss): " << UnitConversion::formatInBestUnit(opp_memory_usage(), "B") << endl;
     }
     else {
         out << "** Event #" << getSimulation()->getEventNumber() << "   t=" << getSimulation()->getSimTime()
             << "   Elapsed: " << timeToStr(getElapsedSecs())
             << progressPercentage() // note: IDE launcher uses this to track progress
             << "   ev/sec=" << speedometer.getEventsPerSec()
-            << "   Memory: " << UnitConversion::formatInBestUnit(opp_memory_usage(), "B") << endl;
+            << "   Memory (rss): " << UnitConversion::formatInBestUnit(opp_memory_usage(), "B") << endl;
     }
 
     // status update is always autoflushed (not only if opt->autoflush is on)
--- src/common/memutil.cc
+++ src/common/memutil.cc
@@ -12,11 +12,10 @@
 *--------------------------------------------------------------*/
 
 #include "memutil.h"
-#include <cstdio>
-#include <cstring>
 
 #ifdef __linux__
 # include <unistd.h>
+# include <sys/resource.h>
 #endif
 
 #ifdef __APPLE__
@@ -34,41 +33,28 @@ namespace common {
 size_t opp_memory_usage()
 {
 #if defined(__linux__)
-    // Sum of resident (VmRSS) and swapped-out (VmSwap) private memory,
-    // in bytes. This corresponds to memory actually used by the process,
-    // regardless of whether it is currently paged in or out.
-    FILE *file = fopen("/proc/self/status", "r");
-    if (!file)
-        return 0;
-    size_t rssKB = 0, swapKB = 0;
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
-        if (strncmp(line, "VmRSS:", 6) == 0)
-            sscanf(line + 6, "%zu", &rssKB);
-        else if (strncmp(line, "VmSwap:", 7) == 0)
-            sscanf(line + 7, "%zu", &swapKB);
-    }
-    fclose(file);
-    return (rssKB + swapKB) * (size_t)1024;
+    // Resident set size (RSS) in bytes. Note: getrusage() does not report
+    // swapped-out memory
+    struct rusage usage;
+    if (getrusage(RUSAGE_SELF, &usage) == 0)
+        return (size_t)usage.ru_maxrss * (size_t)1024;
+    return 0;
 
 #elif defined(__APPLE__)
-    // phys_footprint = resident + compressed + swapped private memory,
-    // i.e. the "Memory" column in Activity Monitor.
-    task_vm_info_data_t vm_info;
-    mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
-    if (KERN_SUCCESS == task_info(mach_task_self(), TASK_VM_INFO,
-                                  (task_info_t)&vm_info, &count))
-        return (size_t)vm_info.phys_footprint;
+    // Resident set size (RSS) in bytes
+    mach_task_basic_info_data_t info;
+    mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+    if (KERN_SUCCESS == task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
+                                  (task_info_t)&info, &count))
+        return (size_t)info.resident_size;
     return 0;
 
 #elif defined(_WIN32)
-    // PrivateUsage = private commit charge (RAM + pagefile),
-    // i.e. the "Commit Size" column in Task Manager.
-    PROCESS_MEMORY_COUNTERS_EX counters;
+    // Resident set size (working set) in bytes
+    PROCESS_MEMORY_COUNTERS counters;
     if (GetProcessMemoryInfo(GetCurrentProcess(),
-                             (PROCESS_MEMORY_COUNTERS *)&counters,
-                             sizeof(counters)))
-        return counters.PrivateUsage;
+                             &counters, sizeof(counters)))
+        return counters.WorkingSetSize;
     return 0;
 
 #else
